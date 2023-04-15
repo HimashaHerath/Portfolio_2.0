5108a7f21b89fa7537e00ebd589e79acac02203b
Himasha Herath <himasha626@gmail.com>
2023-04-15 12:35:41 +0530

    Updates

--- client/src/Sections/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Hero } from "./Hero";
-export { default as Navbar } from "./NavBar";
-export { default as About } from "./About";
-export { default as Works } from "./Works";
-export { default as Feedback } from "./Feedback";
-export { default as Contact } from "./Contact";
