dde7b817e080134fc77be39bd86c95806ed8426f
Himasha <himasha626@gmail.com>
2025-03-28 22:14:59 +0530

    Refactor project structure and update asset paths for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
     assetsDir: './',
   },
-  base: process.env.NODE_ENV === 'production' ? '/Portfolio_2.0/' : '/',
+  // Set base to '/' so assets are referenced from the root domain
+  base: '/',
   publicDir: 'public',
 });
